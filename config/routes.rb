f84634df55c57804f6aa0faff34b4121dfe1b284
Joseph Varghese <joseph.varghese.t@gmail.com>
2023-11-20 14:34:39 -0500

    add sign_up form for the User model

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
   # GET /about
   get "about-us", to: "about#index", as: :about
 
+  # GET /sign-up
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
   # GET /
   root to: "main#index"
 end
