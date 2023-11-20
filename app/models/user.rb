70b10bcd0efda9363dc9f6dda7e32ada2882f8b1
Joseph Varghese <joseph.varghese.t@gmail.com>
2023-11-20 12:48:07 -0500

    validations to the model, as well as some constraints in the schema/db migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@
 
 class User < ApplicationRecord
     has_secure_password :password
+
+    validates :email, presence: true, format: {with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address"}
 end
